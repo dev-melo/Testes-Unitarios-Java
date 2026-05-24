8965d3c545f3a70ba42f8fb2f54e7de032e94bd4
Dev Melo <omelodev@gmail.com>
2026-05-24 18:19:04 -0300

    only push

---
+++ Tests-Erudio/src/test/java/br/com/devmelo/FooBarTest.java
@@ -0,0 +1,12 @@
+package br.com.devmelo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FooBarTest {
+    @Test
+    void test(){
+        fail("Not yet implemented");
+    }
+}
