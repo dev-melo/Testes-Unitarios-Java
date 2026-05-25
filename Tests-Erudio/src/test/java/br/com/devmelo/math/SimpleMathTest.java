a5bbb66aa8513e3c74442ff166109e14a42d13ca
Dev Melo <omelodev@gmail.com>
2026-05-25 07:42:32 -0300

    testSum

---
+++ Tests-Erudio/src/test/java/br/com/devmelo/math/SimpleMathTest.java
@@ -0,0 +1,14 @@
+package br.com.devmelo.math;
+
+import br.dev.devmelo.math.SimpleMath;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class SimpleMathTest {
+    @Test
+    void testSum(){
+        SimpleMath math = new SimpleMath();
+        Double res = math.sum(6.2D, 2D);
+        assertEquals(8.2D, res);
+    }
+}
