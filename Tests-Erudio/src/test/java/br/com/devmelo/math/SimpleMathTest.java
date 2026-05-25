1241c555d490d0b938c5e4dac1c42d65e0f4e530
Dev Melo <omelodev@gmail.com>
2026-05-25 08:45:40 -0300

    criando testes para outras operações de SimpleMath

--- Tests-Erudio/src/test/java/br/com/devmelo/math/SimpleMathTest.java
+++ Tests-Erudio/src/test/java/br/com/devmelo/math/SimpleMathTest.java
@@ -19,4 +19,79 @@ public class SimpleMathTest {
         assertNotEquals(9.2D, actual); // testando se não é igual
         assertNotNull(actual); // testando se existe o objeto
     }
+
+    @Test
+    void testSubtraction() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+
+        Double actual = math.subtraction(firstNumber, secondNumber);
+        var expected = 4.2D;
+
+        assertEquals(expected, actual,
+                () -> firstNumber + " - " + secondNumber + " , not produce " + expected);
+
+        assertNotEquals(10.5D, actual);
+        assertNotNull(actual);
+
+    }
+
+    @Test
+    void testMultiplication() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 6.2D;
+        double secondNumber = 7D;
+
+        Double actual = math.multiplication(firstNumber, secondNumber);
+        var expected = 43.4D;
+
+        assertEquals(expected, actual, () -> firstNumber + " * " + secondNumber + ", not produce " + actual);
+        assertNotNull(actual);
+        assertNotEquals(1D, actual);
+
+    }
+
+    @Test
+    void testDivision() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 12.2D;
+        double secondNumber = 5D;
+
+        Double actual = math.division(firstNumber, secondNumber);
+        var expected = 2.44D;
+
+        assertEquals(expected, actual, () -> firstNumber + " * " + secondNumber + ", not produce " + actual);
+        assertNotNull(actual);
+        assertNotEquals(12D, actual);
+    }
+
+    @Test
+    void testMean() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 12.2D;
+        double secondNumber = 5D;
+
+        Double actual = math.mean(firstNumber, secondNumber);
+        var expected = 30.5D;
+
+        assertEquals(expected, actual, () -> "(" + firstNumber + " * " + secondNumber +")/2" + ", not produce " + actual);
+        assertNotNull(actual);
+        assertNotEquals(13D, actual);
+
+    }
+
+    @Test
+    void testSquareRoot() {
+        SimpleMath math = new SimpleMath();
+        double number = 144D;
+
+        Double actual = math.squareRoot(number);
+        var expected = 12D;
+
+        assertEquals(expected, actual, () -> "The square root of " + number + ", not produce " + actual);
+        assertNotNull(actual);
+        assertNotEquals(13D, actual);
+
+    }
 }
